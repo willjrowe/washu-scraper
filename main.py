fcd10854c004a1b7000972e783a4a3bb75e4e7da
willrowe17 <54510998+willrowe17@users.noreply.github.com>
2020-09-03 23:35:01 -0400

    almost full a&s scrape

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ from firebase import firebase
 import sys
 import time
 from datetime import datetime
+from datetime import date
 now = datetime.now()
 print("Start time")
 print(now)
 args = sys.argv
 
+year = now.strftime("%Y")
+month = now.strftime("%m")
+day = now.strftime("%d")
+today = year + " " + month + " " + day
+
+print(today)
 
 firebase = firebase.FirebaseApplication("https://washu-scrape.firebaseio.com/",None)
 
@@ -20,28 +27,6 @@ driver = webdriver.Chrome(PATH)
 
 driver.get("https://courses.wustl.edu/Semester/Listing.aspx")
 
-# link = driver.find_element_by_link_text(args[1]) #Engineering
-# mainDiv = driver.find_element_by_class_name("CenteredContent")
-# link.click()
-
-# <a id="Body_repSchools_lnkSchool_5" class="ControlLink" href="javascript:__doPostBack('ctl00$Body$repSchools$ctl05$lnkSchool','')" style="font-weight:normal;">Engineering</a>
-
-# try:
-#     element = WebDriverWait(driver,10).until(
-#         EC.presence_of_element_located((By.LINK_TEXT,args[2]))
-#     )
-#     element.click()
-# except:
-#     driver.quit()
-# try:
-#     element = WebDriverWait(driver,10).until(
-#     EC.presence_of_element_located((By.ID,"divSelectRowE8113111E"))
-#     )
-#     print("found")
-# except:
-#     print("not found")
-#     driver.quit()
-# listOfThings = driver.find_elements(By.CSS_SELECTOR,("[crs='131']"))
 departmentTable = driver.find_element_by_id("Body_dlDepartments")
 listOfDepartmentElements = departmentTable.find_elements_by_tag_name("a")
 listofDepartments = []
@@ -50,8 +35,8 @@ for department in listOfDepartmentElements:
     listofDepartments.append(department.text)
 departmentCount=len(listofDepartments)
 for department in listofDepartments:
-    if (department=="BIOLOGY AND BIOMEDICAL SCIENCES(L41)"):
-    # if (department!="All Departments(All)"):
+    # if (department=="BIOLOGY AND BIOMEDICAL SCIENCES(L41)"):
+    if (department!="All Departments(All)"):
         departmentLink = driver.find_element_by_link_text(department)
         departmentLink.click()
         time.sleep(11)
@@ -71,6 +56,9 @@ for department in listofDepartments:
             courseNumber = topLevel[0].text
             courseName = topLevel[1].text
             courseUnits = topLevel[2].text
+            courseNote = "None"
+            if len(topLevel) >= 4:
+                courseNote = topLevel[3].text
             # for topObj in topLevel:
                 # print(topObj.text)
             descLevel = classObj.find_element_by_css_selector("div[class*='DivDetail']")
@@ -78,8 +66,11 @@ for department in listofDepartments:
             actualDesc = actualDesc.get_attribute('textContent')
             attribList = descLevel.find_elements_by_css_selector("a[class^='CrsAttr']")
             #similar to topObh
-            # for attrib in attribList:
-                # print(attrib.get_attribute('textContent'))
+            attribData ={}
+            attrib=0
+            while (attrib+1) < len(attribList):
+                attribData[attribList[attrib].get_attribute('textContent')] = attribList[attrib+1].get_attribute('textContent')
+                attrib+=2
             instrType = descLevel.find_element_by_css_selector("td[style='width:30%;']")
             actualInstr = instrType.find_element_by_tag_name('a')
             actualInstr = actualInstr.get_attribute('textContent')
@@ -93,6 +84,8 @@ for department in listofDepartments:
             sectionArray = []
             for section in sections:
                 sectionContent = section.find_elements(By.CSS_SELECTOR,("td[class*='ItemRow']"))
+                sectionParent = section.find_element_by_xpath('..')
+                # add section description here later not critical hopefully
                 newSection = {
                     "Section" : sectionContent[0].text,
                     "Days" : sectionContent[1].text,
@@ -109,14 +102,17 @@ for department in listofDepartments:
                 "Course Number" : courseNumber,
                 "Course Name" : courseName,
                 "Course Units" : courseUnits,
+                "Course Note" : courseNote,
                 "Course Description" : actualDesc,
                 "Instruction Type" : actualInstr,
                 "Grade Option" : gradeOption,
                 "Frequency" : actualFreq,
-                "Sections" : sectionArray
+                "Course Attributes": attribData,
+                "Sections" : sectionArray,
+                "Date Updated" : today,
             }
             print(classData)
-            # firebase.patch("/artsci/{}/{}".format(department,courseNumber),classData)
+            firebase.patch("/artsci/{}/{}".format(department,courseNumber),classData)
 
 print("end time")            
 done = datetime.now()
--- todo.md
+++ todo.md
@@ -1,5 +1,7 @@
-scrape "lab required" in top level if it exists
-
 scrape section desc and possible other content
 
 rate my professor integration / other washu site integration
+
+something broke with physical education and so russian didnt scrape either
+
+wigs sections are missing
